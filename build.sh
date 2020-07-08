ac80bb91c2022d2440d782ad39996d5957208ff8
ColdIV <yufoolme@gmail.com>
2020-07-09 01:01:04 +0200

    add game name to script

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # @ColdIV
 
+# ask for game name
+read -p "Enter game name [game]: " gamename
+gamename=${gamename:-game}
+
 # removing old published files
 echo "Removing old published files"
 rm -f publish/*
 # zip files for linux
 cd game
 echo "Creating love file"
-zip -9 -x .git/\* -x .\* -q -r game.love .
+zip -9 -x .git/\* -x .\* -q -r ${gamename}.love .
 # create temp folders for win32 and 64
 echo "Creating tmp directories for win32 and win64"
 mkdir -p tmp
@@ -15,23 +19,23 @@ mkdir -p tmp/win32
 mkdir -p tmp/win64
 # create the exe files for both win32 and 64 and copy them in their folder
 echo "Creating exe for win32 and win64"
-mv game.love tmp/
-cat ../win32/love.exe tmp/game.love > tmp/win32/game32.exe
-cat ../win64/love.exe tmp/game.love > tmp/win64/game64.exe
+mv ${gamename}.love tmp/
+cat ../win32/love.exe tmp/${gamename}.love > tmp/win32/${gamename}32.exe
+cat ../win64/love.exe tmp/${gamename}.love > tmp/win64/${gamename}64.exe
 cp ../win32/dll/* tmp/win32/
 cp ../win64/dll/* tmp/win64/
 # zip files for both win32 and 64
 echo "Zipping files for win32 and win64"
 cd tmp/win32
-zip -9 -q -r game32.zip .
+zip -9 -q -r ${gamename}32.zip .
 cd ../win64
-zip -9 -q -r game64.zip .
+zip -9 -q -r ${gamename}64.zip .
 # copy files to publish folder
 echo "Copying win32 and win64 archives to publish directory"
 cd ../../
-mv tmp/game.love ../publish/
-mv tmp/win32/game32.zip ../publish/
-mv tmp/win64/game64.zip ../publish/
+mv tmp/${gamename}.love ../publish/
+mv tmp/win32/${gamename}32.zip ../publish/
+mv tmp/win64/${gamename}64.zip ../publish/
 # remove temp files/folder
 echo "Removing tmp directory"
 rm tmp/win32/*
@@ -41,15 +45,15 @@ rmdir tmp/win64
 rmdir tmp
 # create version for linux without love2d
 echo "Creating version for linux systems"
-cp ../publish/game.love ../tar.gz/lld-11.3/bin/game.love
+cp ../publish/${gamename}.love ../tar.gz/lld-11.3/bin/game.love
 cd ../tar.gz/lld-11.3/
 #tar -cvzf game.tar.gz * > /dev/null
 # zip it
 echo "Zipping files for linux systems"
-zip -9 -q -r game.zip .
+zip -9 -q -r ${gamename}.zip .
 #mv game.tar.gz ../../publish/game.tar.gz
 echo "Moving linux archive to publish directory"
-mv game.zip ../../publish/game.zip
+mv ${gamename}.zip ../../publish/${gamename}.zip
 # remove game.love
 echo "Removing tmp file"
 rm bin/game.love
